f1de81c8f8e8516fabeac117a63be6817edaaa45
al038570 <al038570@s09-pc05>
2015-02-11 19:59:36 +0100

    modificaciones de la base de datos

--- database.sql
+++ database.sql
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de datos: `ausiasyield2014`
+-- Base de datos: `proyectoFinal2015`
 --
 
 -- --------------------------------------------------------
